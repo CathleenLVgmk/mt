268c53965b037ad660270ce8d74c5d1840fc4393
CathleenLVgmk <583854145@qq.com>
2018-11-02 18:21:19 +0800

    middle

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var clean = require('gulp-clean-css');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+var json = require('./src/data.json');
+var server = require('gulp-webserver');
 gulp.task('default', function() {
     return gulp.src('./src/scss/demo.scss')
         .pipe(sass())
@@ -10,4 +15,38 @@ gulp.task('default', function() {
 gulp.task('watch', function() {
     return gulp.watch('./src/scss/demo.scss', gulp.series('default'))
 })
-gulp.task('dev', gulp.series('default', 'watch'));
+
+gulp.task('ser', function() {
+    return gulp.src('./src')
+        .pipe(server({
+            port: 8090,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url, true).pathname;
+                //是文件
+                var paths = path.join('src', pathname);
+                if (path.extname(pathname) != '') {
+                    if (pathname == 'favicon.ico') {
+                        return res.end();
+                    } else {
+                        if (fs.existsSync(paths)) {
+                            res.end(fs.readFileSync(paths));
+                        }
+
+                    }
+                } else {
+                    //是接口
+                    if (pathname == '/') {
+                        res.end(fs.readFileSync('src/index.html'));
+                    } else {
+                        if (pathname == '/api/data') {
+                            res.end(JSON.stringify({
+                                code: 0,
+                                data: json
+                            }))
+                        }
+                    }
+                }
+            }
+        }))
+})
+gulp.task('dev', gulp.series('default', 'ser', 'watch', ));
---
+++ src/js/demo.js
@@ -0,0 +1,135 @@
+var myswiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination'
+    }
+});
+var pages = 1;
+$.ajax({
+    url: '/api/data',
+    success: function(data) {
+        var obj = JSON.parse(data);
+        if (obj.code == 0) {
+            var str1 = '';
+            var str2 = '';
+            var str3 = '';
+            obj.data.list1.forEach(function(file) {
+                str1 += `<li><img src="${file.img}" alt=""><span>${file.title}</span></li>`
+            });
+            obj.data.list2.forEach(function(file) {
+                str2 += `<li><img src="${file.img}" alt=""><span>${file.title}</span></li>`
+            })
+            obj.data.food.forEach(function(file) {
+                str3 += `<li>
+                <div class="des">
+                    <img src="${file.img}" alt="">
+                    <div class="info">
+                        <div class="info-t">
+                            <h3>${file.title}</h3>
+                            <span>${file.des}</span>
+                        </div>
+                        <p>
+                            <span><b>${file.price}</b>元</span>
+                            <em>门市价<i>${file.shopPrice}</i>元</em>
+                        </p>
+                    </div>
+                </div>
+                <div class="count">
+                    <span>已售<em>${file.num}</em></span>
+                </div>
+            </li>`;
+            })
+            $('.list1').html(str1);
+            $('.list2').html(str2);
+            $('.food').html(str3);
+            var myBScroll = new BScroll('.scroll', {
+                probeType: 2
+            });
+
+            // myBScroll.on('scroll', function() {
+            //         if (this.y > $('.pulldown').height()) {
+            //             $('.pulldown').html('释放刷新').addClass('filp');
+            //         } else if (this.y > 40) {
+            //             $('.pulldown').html('下拉刷新').removeClass('flip');
+            //         } else if (this.y < this.maxScrollY - $('.pullup').height()) {
+            //             if (pages <= 3) {
+            //                 $('.pullup').html('上拉加载').removeClass('filp');
+            //             } else {
+            //                 $('.pullup').html('--我也是有底线的--');
+            //             }
+            //         }
+            //     })
+            // myBScroll.on('scrollEnd', function() {
+            //     if ($('.pulldown').hasClass('flip')) {
+            //         $.ajax({
+            //             url: '/api/data',
+            //             success: function(data) {
+            //                 var obj = JSON.parse(data);
+            //                 if (obj.code == 0) {
+            //                     var str3 = '';
+            //                     obj.data.food.forEach(function(file) {
+            //                         str3 += `<li>
+            //                         <div class="des">
+            //                             <img src="${file.img}" alt="">
+            //                             <div class="info">
+            //                                 <div class="info-t">
+            //                                     <h3>${file.title}</h3>
+            //                                     <span>${file.des}</span>
+            //                                 </div>
+            //                                 <p>
+            //                                     <span><b>${file.price}</b>元</span>
+            //                                     <em>门市价<i>${file.shopPrice}</i>元</em>
+            //                                 </p>
+            //                             </div>
+            //                         </div>
+            //                         <div class="count">
+            //                             <span>已售<em>${file.num}</em></span>
+            //                         </div>
+            //                     </li>`;
+            //                     })
+            //                     $('.food').html(str3);
+            //                 }
+            //             }
+            //         })
+            //         $('.pulldown').html('下拉刷新').removeClass('flip');
+            //     } else if ($('.pullup').hasClass('filp')) {
+            //         if (pages <= 3) {
+            //             pages++;
+            //             $.ajax({
+            //                 url: '/api/data',
+            //                 success: function(data) {
+            //                     var obj = JSON.parse(data);
+            //                     if (obj.code == 0) {
+            //                         var str3 = $('.food').html();
+            //                         obj.data.food.forEach(function(file) {
+            //                             str3 += `<li>
+            //                             <div class="des">
+            //                                 <img src="${file.img}" alt="">
+            //                                 <div class="info">
+            //                                     <div class="info-t">
+            //                                         <h3>${file.title}</h3>
+            //                                         <span>${file.des}</span>
+            //                                     </div>
+            //                                     <p>
+            //                                         <span><b>${file.price}</b>元</span>
+            //                                         <em>门市价<i>${file.shopPrice}</i>元</em>
+            //                                     </p>
+            //                                 </div>
+            //                             </div>
+            //                             <div class="count">
+            //                                 <span>已售<em>${file.num}</em></span>
+            //                             </div>
+            //                         </li>`;
+            //                         })
+            //                         $('.food').html(str3);
+            //                     }
+            //                 }
+            //             })
+            //             $('.pullup').html('上拉加载').removeClass('flip');
+            //         } else {
+            //             $('.pullup').html('--我也是有底线的--');
+            //         }
+            //     }
+            // })
+        }
+    }
+})
